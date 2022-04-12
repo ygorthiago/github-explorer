d615f9bb88e4a2ca1f30d9ec99f7f6bf9b298db8
Ygor Thiago Farias <ygorthiagoff@outlook.com>
2022-04-12 17:21:14 -0300

    refactor: use useRepositoriesHooks and repository components

--- src/pages/Repository/Repository.tsx
+++ src/pages/Repository/Repository.tsx
@@ -1,59 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Link, useMatch } from 'react-router-dom';
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
-import { BiGitPullRequest } from 'react-icons/bi';
-import { VscIssues } from 'react-icons/vsc';
-import api from '../../services/api';
+import { FiChevronLeft } from 'react-icons/fi';
 
-import { Header, RepositoryInfo, Issues } from './styles';
+import { useRepositoriesHook } from '../../hooks/useRepositories';
 
-interface Repository {
-  full_name: string;
-  html_url: string;
-  description: string;
-  stargazers_count: number;
-  forks_count: number;
-  open_issues_count: number;
-  watchers_count: number
-  owner: {
-    login: string;
-    avatar_url: string;
-  };
-}
-
-interface Issue {
-  id: number;
-  title: string;
-  html_url: string;
-  pull_request: Object;
-  user: {
-    login: string;
-  };
-}
+import { Header } from './styles';
+import { RepositoryInfo } from '../../components/Repository/RepositoryInfo';
+import { RepositoryIssues } from '../../components/Repository/RepositoryIssues';
 
 export function Repository() {
-  const [repository, setRepository] = useState<Repository | null>(null);
-  const [issues, setIssues] = useState<Issue[]>([]);
+  const {
+    getRepository,
+    getRepositoryIssues,
+    repository,
+    issues,
+    isGetRepositoryLoading,
+    isGetRepositoryError,
+    isGetRepositoryIssuesError,
+    isGetRepositoryIssuesLoading
+  } = useRepositoriesHook()
 
   const match = useMatch('/repository/:repository*');
 
-  const pathName = match?.pathname.replace('/repository/', '')
+  const repositoryName = match?.pathname.replace('/repository/', '')!;
 
   useEffect(() => {
-    api.get(`/repos/${pathName}`).then(response => {
-      setRepository(response.data);
-    });
+    if (repositoryName) {
+      getRepository(repositoryName)
+    }
+  }, [repositoryName]);
 
-    api.get(`/repos/${pathName}/issues`, {
-      params: {
-        page: 1,
-        per_page: 10,
-        sort: 'updated-desc'
-      }
-    }).then(response => {
-      setIssues(response.data);
-    });
-  }, [pathName]);
+  useEffect(() => {
+    if (repositoryName && repository?.open_issues_count) {
+      getRepositoryIssues(repositoryName)
+    }
+  }, [repositoryName, repository?.open_issues_count]);
 
   return (
     <>
@@ -64,53 +45,20 @@ export function Repository() {
         </Link>
       </Header>
 
-      {repository && (
-        <RepositoryInfo>
-          <header>
-            <img
-              src={repository.owner.avatar_url}
-              alt={repository.owner.login}
-            />
-            <div>
-              <a href={repository.html_url} target="_blank">{repository.full_name}</a>
-              <p>{repository.description}</p>
-            </div>
-          </header>
-          <ul>
-            <li>
-              <strong>{repository.stargazers_count}</strong>
-              <span>Stars</span>
-            </li>
-            <li>
-              <strong>{repository.forks_count}</strong>
-              <span>Forks</span>
-            </li>
-            <li>
-              <strong>{repository.open_issues_count}</strong>
-              <span>Issues/PRs open</span>
-            </li>
-            <li>
-              <strong>{repository.watchers_count}</strong>
-              <span>Watchers</span>
-            </li>
-          </ul>
-        </RepositoryInfo>
-      )}
+      <RepositoryInfo
+        repository={repository}
+        isLoading={isGetRepositoryLoading}
+        isError={isGetRepositoryError}
+        retryFunction={() => getRepository(repositoryName)}
+      />
 
-      {!!issues.length && (
-      <Issues>
-        <h2>Trending open issues/pull requests</h2>
-        {issues.map(issue => (
-          <a key={issue.id} href={issue.html_url} target="_blank">
-            {issue.pull_request ? <BiGitPullRequest /> : <VscIssues />}
-            <div>
-              <strong>{issue.title}</strong>
-              <p>{issue.user.login}</p>
-            </div>
-            <FiChevronRight size={20} />
-          </a>
-        ))}
-      </Issues>
+      {repository && (
+        <RepositoryIssues
+          repositoryIssues={issues}
+          isLoading={isGetRepositoryIssuesLoading}
+          isError={isGetRepositoryIssuesError}
+          retryFunction={() => getRepositoryIssues(repositoryName)}
+        />
       )}
     </>
   );
