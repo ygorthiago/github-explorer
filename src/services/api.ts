b81c164bba143cb0e89cc0305dd30b6165e4cf60
Ygor Thiago Farias <ygorthiagoff@outlook.com>
2022-04-12 17:17:31 -0300

    feat: add v3 header

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.github.com',
   headers: {
-    Accept: 'application/vnd.github.v3+json'
+    Accept: 'application/vnd.github.v3+json',
   }
 });
 
