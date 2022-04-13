1a22be307a1ef044c317eeb74fcafaaac6b14707
Ygor Thiago Farias <ygorthiagoff@outlook.com>
2022-04-13 00:53:07 -0300

    feat: api response time

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,4 +7,31 @@ const api = axios.create({
   }
 });
 
+let requestTimeStart = 0;
+
+axios.interceptors.request.use(
+  function (config) {
+    requestTimeStart = new Date().getMilliseconds();
+    return config;
+}, function (error) {
+    return Promise.reject(error);
+});
+
+api.interceptors.response.use(
+  (response) => {
+    const requestTimeEnd = new Date().getMilliseconds();
+    const responseTime =  requestTimeEnd - requestTimeStart;
+
+    const resWithResponseTime = {
+      ...response,
+      responseTime
+    }
+    
+    return resWithResponseTime;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
