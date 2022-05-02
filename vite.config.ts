d7d958fd6d826bcc388ddb39b0a4c5d88fd1d993
Ygor Thiago Farias <ygorthiagoff@outlook.com>
2022-05-01 21:17:33 -0300

    chore: app configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const envWithProcessPrefix = Object.entries(env).reduce(
+    (prev, [key, val]) => {
+      return {
+        ...prev,
+        ["process.env." + key]: `"${val}"`,
+      };
+    },
+    {}
+  );
+
+  return {
+    plugins: [react()],
+    define: envWithProcessPrefix,
+  };
+});
